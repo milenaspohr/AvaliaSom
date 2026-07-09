04964c7f933b38bba7caf16f9bdf54dbc9e0c358
milenaspohr <milena.spohr@aluno.feliz.ifrs.edu.br>
2026-07-09 14:33:46 -0300

    Change database name from 'musicBoxd' to 'avaliasom'

--- bd.js
+++ bd.js
@@ -4,7 +4,7 @@ const conexao = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "musicBoxd"
+    database: "avaliasom"
 });
 
 conexao.connect((erro) => {
